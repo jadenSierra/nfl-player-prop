7397a5e961db9ec7cc07371cd4eb1c1fc9f5f75c
Jaden Sierra <Jadensierra31@gmail.com>
2025-06-06 16:14:58 -0500

    initial commit

---
+++ src/main/java/com/project/nfl_player_prop/NflPlayerPropApplication.java
@@ -0,0 +1,14 @@
+package com.project.nfl_player_prop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NflPlayerPropApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(NflPlayerPropApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/nfl_player_prop/NflPlayerPropApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.nfl_player_prop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NflPlayerPropApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
